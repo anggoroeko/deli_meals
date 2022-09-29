ca4ae1684e59691b103798e01551e1f7b5ff853c
Anggoro <anggoroeko17@gmail.com>
2022-09-29 16:07:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import './categories_screen.dart';
+import './category_meals_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Deli Meals',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(),
+          fontFamily: "Raleway",
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyMedium: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleLarge: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: "RobotoCondensed",
+                  fontWeight: FontWeight.bold))),
+      home: const CategoriesScreen(),
+      routes: {'/category-meals': (context) => CategoryMealsScreen()},
+    );
+  }
+}
